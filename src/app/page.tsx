5dd3d313368641c8f80f6644039d2b8cc2a8b37e
Yadong Xie <vthinkxie@gmail.com>
2023-11-17 21:18:58 +0800

    feat: add layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,119 @@
 "use client";
+import type { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/types";
 import dynamic from "next/dynamic";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { debounceTime, Subject, distinctUntilChanged } from "rxjs";
 const Excalidraw = dynamic(
   async () => (await import("@excalidraw/excalidraw")).Excalidraw,
   {
     ssr: false,
   },
 );
+
+function blobToBase64(blob: Blob): Promise<string> {
+  return new Promise((resolve, _) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result as string);
+    reader.readAsDataURL(blob);
+  });
+}
+
 export default function Home() {
+  const excalidrawAPI = useRef<ExcalidrawImperativeAPI | null>(null);
+  const change$ = useRef(new Subject<void>());
+  const base64$ = useRef(new Subject<string>());
+  const [initialed, setInitialed] = useState(false);
+  const [imgSrc, setImgSrc] = useState<string>("");
+  const getBase64 = useCallback(async () => {
+    const exportToBlob = (await import("@excalidraw/excalidraw")).exportToBlob;
+    if (!excalidrawAPI.current) {
+      return;
+    }
+    const elements = excalidrawAPI.current.getSceneElements();
+    if (!elements || !elements.length) {
+      return;
+    }
+    const blob = await exportToBlob({
+      elements,
+      files: excalidrawAPI.current.getFiles(),
+      getDimensions: () => {
+        return { width: 1024, height: 1024 };
+      },
+    });
+    const base64 = await blobToBase64(blob);
+    return base64.replace(/^data:image\/(png|jpg);base64,/, "");
+  }, []);
+
+  useEffect(() => {
+    if (excalidrawAPI.current) {
+      excalidrawAPI.current.onChange(() => {
+        change$.current.next();
+      });
+    }
+  }, [initialed]);
+
+  useEffect(() => {
+    const subscription = change$.current
+      .pipe(debounceTime(300))
+      .subscribe(async () => {
+        const base64 = await getBase64();
+        if (base64) {
+          base64$.current.next(base64);
+        }
+      });
+    return () => subscription.unsubscribe();
+  }, [getBase64]);
+
+  useEffect(() => {
+    const subscription = base64$.current
+      .pipe(distinctUntilChanged())
+      .subscribe((base64) => {
+        const body = {
+          guidance_scale: 8,
+          height: 1024,
+          input_image: base64,
+          lcm_steps: 50,
+          prompt: "Very majestic rooster",
+          seed: 2159232,
+          steps: 4,
+          strength: 0.8,
+          width: 1024,
+        };
+        fetch("https://stable.bjz.edr.lepton.ai/run", {
+          headers: {
+            accept: "image/jpeg",
+            authorization: "Bearer 8ee43d8a2aa24f3083336ac96bd12afb",
+            "content-type": "application/json",
+            "x-lepton-deployment": "imgpilot",
+          },
+          referrer: "https://dashboard.lepton.ai/",
+          body: JSON.stringify(body),
+          method: "POST",
+          mode: "cors",
+          credentials: "include",
+        })
+          .then((data) => data.blob())
+          .then((data) => {
+            setImgSrc(URL.createObjectURL(data));
+          });
+      });
+    return () => subscription.unsubscribe();
+  }, []);
   return (
-    <div className="h-screen">
-      <Excalidraw></Excalidraw>
+    <div className="inset-0 absolute">
+      <div className="h-full w-full relative lg:flex">
+        <div className="w-full h-full lg:w-1/2 bg-zinc-100 flex items-center justify-center">
+          {imgSrc && <img alt="img" height={1024} width={1024} src={imgSrc} />}
+        </div>
+        <div className="w-full lg:w-1/2 h-full">
+          <Excalidraw
+            excalidrawAPI={(api) => {
+              excalidrawAPI.current = api;
+              setInitialed(true);
+            }}
+          ></Excalidraw>
+        </div>
+      </div>
     </div>
   );
 }
