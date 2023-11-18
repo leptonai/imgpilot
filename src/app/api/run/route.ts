85aa057f70df7675230e4ed2da893ff13ee77a12
Yadong Xie <vthinkxie@gmail.com>
2023-11-19 00:55:37 +0800

    feat: local version

--- src/app/api/run/route.ts
+++ src/app/api/run/route.ts
@@ -1,34 +1,13 @@
 import { NextRequest } from "next/server";
-import { Ratelimit } from "@upstash/ratelimit";
-import { kv } from "@vercel/kv";
-
-const API_URL = process.env.API_URL || "localhost:8080";
-const API_TOKEN = process.env.API_TOKEN || "Invalid";
-
-const ratelimit = new Ratelimit({
-  redis: kv,
-  // 10 requests from the same IP in 1 second
-  limiter: Ratelimit.slidingWindow(10, "1 s"),
-});
 
 export async function POST(req: NextRequest) {
-  const ip = req.ip ?? "::1";
-  const { remaining } = await ratelimit.limit(ip);
-
-  if (remaining < 1) {
-    return new Response("Too many requests", {
-      status: 429,
-    });
-  }
-
   const headers = new Headers();
   headers.set("Accept", `image/jpeg`);
-  headers.set("Authorization", `Bearer ${API_TOKEN}`);
   headers.set(
     "Content-Type",
     req.headers.get("Content-Type") || "application/json",
   );
-  const url = new URL("/run", API_URL);
+  const url = new URL("/run", "http://0.0.0.0:8080");
   const body = await req.text();
 
   const res = await fetch(url.toString(), {
